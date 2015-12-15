66c62cbe38f39d3ba6690734555bc25c2120c9b1
Qixing  Huang <pqx.huang@gmail.com>
2015-12-15 04:56:31 -0600

    added knn search

--- pose_estimation/cam_nearest_query.m
+++ pose_estimation/cam_nearest_query.m
@@ -2,15 +2,25 @@ function [Shapes_knn] = cam_nearest_query(Image, Camera, shape_folder, knn)
 % This function finds the nearest neighbors of an image object among a
 % collection of shapes stored in a shape folder. The shapes are assumed to
 % be consistently oriented and scaled.
+% Input arguments:
+%   Image: the input image object
+%   Camera: the associated camera configuration
+%   shape_folder: the shape folder that stores all the input shapes, The
+%                 shapes are assumed to be consistently oriented, and the
+%                 length of the diagonal of each bounding box is 1 
+%                 (the default setting in pose estimation) 
+%   knn: the number of nearest-neighbors for a given image object
+% Output argument:
+%   Shapes_knn: the retrieved nearest neighbors
 
-patch = imResample(single(Image.im), [360, 360])/255;
-H = hog(patch, 60, 16);
-query_hog = reshape(H, [2304, 1]);
+patch = imResample(single(Image.im), [400, 400])/255;
+H = hog(patch, 100, 16);
+query_hog = reshape(H, [1024, 1]);
 
 temp = dir(shape_folder);
 numShapes = length(temp)-2;
 
-shapes_hog = single(zeros(2304, numShapes));
+shapes_hog = single(zeros(1024, numShapes));
 
 
 for shapeId = 1 : numShapes
@@ -23,15 +33,12 @@ d = query_hog*ones(1, numShapes) - shapes_hog;
 [s, ids] = sort(sum(d.*d));
 ids = ids(1:knn);
 
-figure(1);
 for i = 1:knn
     shapeId = ids(i);
     load([shape_folder, temp(shapeId+2).name]);
     Shape.has_material = 1;
     Shapes_knn{i} = Shape;
     images{i} = cam_render_shape(Shape, Camera);
-    subplot(6,8, i);
-    imshow(images{i});
 end
 
 function [H] = hos_process(shape_folder, temp, Camera, shapeId)
